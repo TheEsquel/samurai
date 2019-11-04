5fddf01e0d5270a87c84e3c09ce2356207eb8d1e
TheEsquel <rem.asadullin@gmail.com>
2019-11-04 13:10:50 +0500

    initial

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://st2.depositphotos.com/5142301/7567/v/950/depositphotos_75677235-stock-illustration-lion-head-logo.jpg"
+                alt=""/>
+        </header>
+    )
+
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../App.css';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <img src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" alt=""/>
+            <div>
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0_2nqm0H20gpO-Pf9BsBwuAYt3McWcb-6rFs37i244h71Lyrnkg&s"
+                    alt=""/>
+            </div>
+            <div>
+                my posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    posts
+                </div>
+            </div>
+        </div>
+    )
+
+};
+
+export default Profile
